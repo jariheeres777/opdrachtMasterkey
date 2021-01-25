bc6e1f04ff26ae56ec746f9ed5412d458aa6949f
jariheeres777 <66300860+jariheeres777@users.noreply.github.com>
2021-01-25 21:28:20 +0100

    klaar met de opdracht

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args) throws IOException
-    {
+public class Main {
+    public static void main(String[] args) throws IOException {
         // write your code here
         //input filepath dit gaat op de naam van het bestand dit werkt alleen als alle bestand namen opgeslagen zijn in dezelfde map
         System.out.println("vul de naam van het bestand dat je wil uitlezen in");
@@ -18,35 +17,58 @@ public class Main
         // locatie waar het bestand is opgeslagen dit heb ik als losse variable opgeslagen
         String bestandLocatie = "C:/Users/jarih/Downloads/";
 
-        File f1=new File(bestandLocatie+userName+".txt"); //Creation of File Descriptor for input file
+        File f1 = new File(bestandLocatie + userName + ".txt"); //Creation of File Descriptor for input file
         //input het woord waar op gefilterd
         System.out.println("vul de naam van het bestand dat je wil uitlezen in");
         Scanner wordName = new Scanner(System.in);
         String filterWord = wordName.nextLine();
 
-        String[] words=null;  //Intialize the word Array
+        System.out.println("wil je ook als het een deel van het woord is dat het dan meegeteld word?");
+        Scanner vraag1 = new Scanner(System.in);
+        String vraag1antwoord = vraag1.nextLine();
+
+        System.out.println("moet het hoofdletter gevoelig zijn?");
+        Scanner vraag2 = new Scanner(System.in);
+        String vraag2antwoord = vraag2.nextLine();
+
+
+        String[] words = null;  //Intialize the word Array
         FileReader fr = new FileReader(f1);  //Creation of File Reader object
         BufferedReader br = new BufferedReader(fr); //Creation of BufferedReader object
         String s;
-        String input=filterWord;   // Input word to be searched
-        int count=0;   //Intialize the word to zero
-        while((s=br.readLine())!=null)   //Reading Content from the file
+        String input = filterWord;   // Input word to be searched
+        int count = 0;   //Intialize the word to zero
+        while ((s = br.readLine()) != null)   //Reading Content from the file
         {
-            words=s.split(" ");  //Split the word using space
-            for (String word : words)
-            {
-                if (word.equals(input))   //Search for the given word
-                {
-                    count++;    //If Present increase the count by one
+            words = s.split(" ");  //Split the word using space
+            for (String word : words) {
+                if (vraag1antwoord.equals("nee")){
+                    if(vraag2antwoord.equals("ja")){
+                        if (word.equals(input))   //Search for the given word
+                        {
+                            count++;    //If Present increase the count by one
+                        }
+                    }
+
                 }
+                if(vraag1antwoord.equals("ja")){
+                    if (word.contains(input)) {
+                        count++;
+                    }
+                }
+                if(vraag2antwoord.equals("nee")){
+                    if (word.equalsIgnoreCase(input)) {
+                        count++;
+                    }
+                }
+
             }
         }
-        if(count!=0)  //Check for count not equal to zero
+
+        if (count != 0)  //Check for count not equal to zero
         {
-            System.out.println("de naam van het gegeven woord komt "+count+ " voor in het bestand");
-        }
-        else
-        {
+            System.out.println("de naam van het gegeven woord komt " + count + " voor in het bestand");
+        } else {
             System.out.println("de naam van het gegeven woord komt niet voor in het bestand");
         }
 
